2f6403e477ea7f34abe0f66ed004b25b5d9cf82e
Giovanni Leon <giovanni.r.leon@gmail.com>
2025-07-12 12:39:05 -0500

    added echos

--- git_commit.sh
+++ git_commit.sh
@@ -8,39 +8,41 @@ if [[ -z "$1" ]]; then
     exit 1
 fi
 
-
+echo ""
 # Show status before
 echo "Git status before committing:"
 git status -s
 
-
+echo ""
 # Add all changes
 echo "Staging all changes…"
 git add -A
 
-
+echo ""
 # Commit
 echo "Committing with message: $1"
 git commit -m "$1"
 
-
 if [[ $? -ne 0 ]]; then
+    echo ""
     echo "Commit failed. Possibly nothing to commit."
     exit 1
 fi
 
-
+echo ""
 # Ask if they want to push
 read -p "Do you want to push to remote as well? (y/n): " yn
 case $yn in
     [Yy]* )
+        echo ""
         echo "Pushing to remote…"
         git push
         ;;
     * )
+        echo ""
         echo "Commit complete. Not pushing to remote."
         ;;
 esac
 
-
+echo ""
 echo "Done."
