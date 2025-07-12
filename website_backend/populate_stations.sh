376d3f18abf8ac84da9059d36cf9d0d408189b22
Giovanni Leon <giovanni.r.leon@gmail.com>
2025-07-12 17:13:53 -0500

    added population script for stations

---
+++ website_backend/populate_stations.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# as of 7/12/25, there are 22 stations, 4 table tops ones and 18 (1 rack)
+
+URL='https://backend.tss.wistronlabs.com/api/v1/stations'
+
+for i in {1..4}; do
+    curl -X POST "$URL" \
+        -H "Content-Type: application/json" \
+        -d "{\"station_name\": \"Stn $i\"}"
+done
+
+for i in {110..118}; do
+    curl -X POST "$URL" \
+        -H "Content-Type: application/json" \
+        -d "{\"station_name\": \"Stn $i\"}"
+done
+
+for i in {127..136}; do
+    curl -X POST "$URL" \
+        -H "Content-Type: application/json" \
+        -d "{\"station_name\": \"Stn $i\"}"
+done
+
