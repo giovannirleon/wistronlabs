78843b25f2db4544096c8815e2231467181d559f
Giovanni Leon <giovanni.r.leon@gmail.com>
2025-12-08 20:30:40 -0500

    added location information to backend

--- website_backend/src/routes/server.js
+++ website_backend/src/routes/server.js
@@ -5,9 +5,16 @@ const router = express.Router();
 router.get("/time", (req, res) => {
   const now = new Date();
 
+  const timeZone =
+    process.env.LOCATION === "TSS"
+      ? "America/Chicago"
+      : process.env.LOCATION === "FRK"
+      ? "America/New_York"
+      : "UTC";
+
   // Format explicitly to America/Chicago
   const cstFormatter = new Intl.DateTimeFormat("en-US", {
-    timeZone: "America/Chicago",
+    timeZone: timeZone,
     year: "numeric",
     month: "2-digit",
     day: "2-digit",
@@ -23,7 +30,7 @@ router.get("/time", (req, res) => {
     timestamp: now.getTime(), // epoch ms
     isoTime: now.toISOString(), // ISO in UTC
     localtime: cstTime, // human-readable CST/CDT
-    zone: "America/Chicago",
+    zone: timeZone, // time zone name
   };
 
   res.json(response);
